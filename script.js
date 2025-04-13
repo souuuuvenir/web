fe3820b383b88a39e8c3adcea51b53ef55610578
souuuuvenir <lch23413070030521@cecyteh.edu.mx>
2025-04-12 19:02:52 -0600

    Update script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const swiper = new Swiper('.mySwiper', {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 20,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
+});
